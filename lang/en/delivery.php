3a0e7331f8501d9f889fcca937421b35d27f2a5a
KyryloRiznychenko <kirill.reznichenkoo@gmail.com>
2024-04-17 11:46:55 +0000

    I added a new translates for the  delivery func.

---
+++ lang/en/delivery.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'errors' => [
+        'default_address_is_not_setup' => 'You must to setup default delivery address.',
+        'invalid_service_name' => 'Invalid Delivery Service Name.',
+        'plug' => 'Oops... something is going wrong. Try later again.'
+    ],
+];
