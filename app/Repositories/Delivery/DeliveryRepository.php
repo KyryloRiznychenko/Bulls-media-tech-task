9632ad2d41c8c994dc8e85e219d834eece9984b2
KyryloRiznychenko <kirill.reznichenkoo@gmail.com>
2024-04-17 14:22:39 +0000

    I replaced the JsonException to the default Exception

--- app/Repositories/Delivery/DeliveryRepository.php
+++ app/Repositories/Delivery/DeliveryRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Delivery;
 
 use App\Models\Delivery;
+use Exception;
 use Illuminate\Support\Facades\DB;
 use JsonException;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 class DeliveryRepository
 {
     /**
-     * @throws JsonException
+     * @throws Exception
      */
     public function store(array $inputData): Delivery
     {
@@ -19,10 +20,10 @@ class DeliveryRepository
             $delivery = Delivery::create($inputData);
 
             DB::commit();
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             DB::rollBack();
 
-            throw new JsonException(
+            throw new Exception(
                 "{$e->getLine()} \n{$e->getMessage()}",
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
@@ -32,9 +33,9 @@ class DeliveryRepository
     }
 
     /**
-     * @throws JsonException
+     * @throws Exception
      */
-    public function update(string $uuid, array $inputData)
+    public function update(string $uuid, array $inputData): Delivery
     {
         DB::beginTransaction();
         try {
@@ -42,9 +43,9 @@ class DeliveryRepository
             $delivery->update($inputData);
 
             DB::commit();
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             DB::rollBack();
-            throw new JsonException(
+            throw new Exception(
                 "{$e->getLine()} \n{$e->getMessage()}",
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
